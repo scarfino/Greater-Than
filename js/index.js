b631af750f17f6978d3a3d8aafb7863c89b1db0b
scarfino <35802058+scarfino@users.noreply.github.com>
2018-05-24 22:37:41 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//is number1 greater than number2?
+function isGreaterThan(numberOne, numberTwo) {
+  return number * number;
+  if (numberOne > numberTwo) {
+    return true;
+  } else {
+    return false;
+  }
+}
